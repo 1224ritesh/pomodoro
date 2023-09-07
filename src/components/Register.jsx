2d6350b347fc33a7f255abea5a8a39568bf35f48
Ritesh Sharma <98305600+1224ritesh@users.noreply.github.com>
2023-09-07 12:23:01 +0530

    changes

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -48,7 +48,7 @@ const Register = () => {
       <div className="flex items-center justify-center min-h-screen bg-gray-100">
         <div className="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
           <div className="flex flex-col justify-center p-8 md:p-14">
-            <span className="mb-3 text-4xl font-bold">PMODORO</span>
+            <span className="mb-3 text-4xl font-bold">POMODORO</span>
             <span className="font-light text-gray-400 mb-8">
               You are here to Focus!
             </span>
